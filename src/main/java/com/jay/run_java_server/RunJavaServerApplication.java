704cb462c12a2f0e61e71ebb5ba70bd2e1182cc6
Talentov87 <talentovjay@gmail.com>
2024-07-07 00:09:28 +0530

    Spring Boot App Init

---
+++ src/main/java/com/jay/run_java_server/RunJavaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jay.run_java_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunJavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunJavaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jay/run_java_server/RunJavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.run_java_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunJavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
